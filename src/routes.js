6ac078fb6ae29f4e5c22b35e234e0e9d47d2f75e
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-01-15 16:51:43 -0300

    insert coordinates on dev post

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ routes.get('/', (request, response) => {
 });
 
 routes.post('/devs', async (request, response) => {
-  const { github_username, techs } = request.body;
+  const { github_username, techs, latitude, longitude } = request.body;
 
   const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
@@ -17,12 +17,18 @@ routes.post('/devs', async (request, response) => {
 
   const techsArray = techs.split(',').map(tech => tech.trim());
 
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  }
+
   const dev = await Dev.create({
     github_username,
     name,
     avatar_url,
     bio,
     techs: techsArray,
+    location,
   });
 
   return response.json(dev);
