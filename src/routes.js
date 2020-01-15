3a4eecc1896a9cde393e2fdd729b141ef2528bb6
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-01-15 17:25:04 -0300

    search module

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+
 const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
@@ -7,6 +9,9 @@ routes.get('/', (request, response) => {
   return response.json({ message: 'Hello' });
 });
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
